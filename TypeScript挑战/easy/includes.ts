2ae9de22f25acf8fb77f4b2a9fcd1141bed2ef1c
wangtunan <why583440138@gmail.com>
2022-01-02 23:12:57 +0800

    README.md更新/简单章节文章勘误

--- TypeScript挑战/easy/includes.ts
+++ TypeScript挑战/easy/includes.ts
@@ -1,14 +1,30 @@
 import { Equal, Expect } from '../index'
 // 用法：实现Array.prototype.includes方法
-type MyIncludes<T extends readonly any[], U> = U extends T[number] ? true : false 
+// 简单版
+// type MyIncludes<T extends readonly any[], U> = U extends T[number] ? true : false
+// 完善版
+type MyIncludes<T extends readonly any[], U> = 
+  T extends [infer R, ...infer L]
+    ? Equal<R, U> extends true
+      ? true
+      : MyIncludes<L, U>
+    : false
 
 // example
 type includesResult = MyIncludes<[1, 2, 3], 1>
 
 // test
 type testCases = [
+  Expect<Equal<MyIncludes<['Kars', 'Esidisi', 'Wamuu', 'Santana'], 'Kars'>, true>>,
+  Expect<Equal<MyIncludes<['Kars', 'Esidisi','Wamuu', 'Santana'], 'Dio'>, false>>,
+  Expect<Equal<MyIncludes<[1, 2, 3, 5, 6, 7], 7>, true>>,
+  Expect<Equal<MyIncludes<[1, 2, 3, 5, 6, 7], 4>, false>>,
+  Expect<Equal<MyIncludes<[1, 2, 3], 2>, true>>,
   Expect<Equal<MyIncludes<[1, 2, 3], 1>, true>>,
-  Expect<Equal<MyIncludes<[1, 2, 3], '1'>, false>>,
-  Expect<Equal<MyIncludes<['AAA', 'BBB', 'CCC'], 'AAA'>, true>>,
-  Expect<Equal<MyIncludes<['AAA', 'BBB', 'CCC'], 'DDD'>, false>>
+  Expect<Equal<MyIncludes<[{}], { a: 'A' }>, false>>,
+  Expect<Equal<MyIncludes<[boolean, 2, 3, 5, 6, 7], false>, false>>,
+  Expect<Equal<MyIncludes<[true, 2, 3, 5, 6, 7], boolean>, false>>,
+  Expect<Equal<MyIncludes<[false, 2, 3, 5, 6, 7], false>, true>>,
+  Expect<Equal<MyIncludes<[{ a: 'A' }], { readonly a: 'A' }>, false>>,
+  Expect<Equal<MyIncludes<[{ readonly a: 'A' }], { a: 'A' }>, false>>,
 ]
